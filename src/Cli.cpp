1bdeeaa61cc536ff150293483dd6fab2d141a649
lukemcelya <johnlukemcelya@gmail.com>
2026-02-28 21:40:54 -0600

    Add .gitignore

---
+++ src/Cli.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Luke on 2/28/2026.
+//
+
+#include "Cli.h"
---
+++ src/Cli.h
@@ -0,0 +1,14 @@
+//
+// Created by Luke on 2/28/2026.
+//
+
+#ifndef CPPPREP_CLI_H
+#define CPPPREP_CLI_H
+
+
+class Cli
+{
+};
+
+
+#endif //CPPPREP_CLI_H
